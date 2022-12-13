b13a268f3e244d4c1470d6a5216efc335ebe0b9a
chirs <yosoypro_117@hotmail.com>
2022-12-12 22:04:51 -0600

    legibilidad

--- curso-practico/main.php
+++ curso-practico/main.php
@@ -9,11 +9,11 @@ for($i = 0; $i < 3; $i++) {
 print_r($letras);
 
 echo "
-<form action='evaluador.php'>
-    <input type='text' name='palabra0'/>
-    <input type='text' name='palabra1'/>
-    <input type='text' name='palabra2'/>
-    <button type='submit'>Enviar</button>
+<form action = 'evaluador.php'>
+    <input type = 'text' name = 'palabra0'/>
+    <input type = 'text' name = 'palabra1'/>
+    <input type = 'text' name = 'palabra2'/>
+    <button type = 'submit'>Enviar</button>
 </form>
 ";
 ?>
