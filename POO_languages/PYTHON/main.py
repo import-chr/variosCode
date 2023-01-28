2afaf4ea7eae269494221ec72030455093a5447b
chirs <yosoypro_117@hotmail.com>
2023-01-27 22:41:50 -0600

    instances of Car

--- POO_languages/PYTHON/main.py
+++ POO_languages/PYTHON/main.py
@@ -1,2 +1,17 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hello World")
+
+    car0 = Car()
+    car0.driver = "David"
+    car0.license = "QWE123"
+    car0.passenger = 2
+
+    car1 = Car()
+    car1.driver = "Luis"
+    car1.license = "TRY987"
+    car1.passenger = 4
+
+    print(vars(car0))
+    print(vars(car1))
