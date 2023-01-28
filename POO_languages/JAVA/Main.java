f78186bef84d7f77dcf46e0e20f91bb6b1539109
chirs <yosoypro_117@hotmail.com>
2023-01-27 22:41:17 -0600

    instances of Car class

--- POO_languages/JAVA/Main.java
+++ POO_languages/JAVA/Main.java
@@ -1,5 +1,18 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello Wolrd");
+
+        Car car0 = new Car();
+        car0.driver = "David";
+        car0.license = "QWE123";
+        car0.passenger = 2;
+
+        Car car1 = new Car();
+        car1.driver = "Luis";
+        car1.license = "TRY987";
+        car1.passenger = 4;
+
+        car0.printData();
+        car1.printData();
     }
 }
