0a8633e5bb6eed51f3cef83dac5afcddd63225cd
chirs <yosoypro_117@hotmail.com>
2023-02-08 13:07:05 -0600

    init py

--- POO_languages/PYTHON/main.py
+++ POO_languages/PYTHON/main.py
@@ -1,17 +1,10 @@
+from account import Account
 from car import Car
 
 if __name__ == "__main__":
     print("Hello World")
 
-    car0 = Car()
-    car0.driver = "David"
-    car0.license = "QWE123"
-    car0.passenger = 2
-
-    car1 = Car()
-    car1.driver = "Luis"
-    car1.license = "TRY987"
-    car1.passenger = 4
+    car0 = Car("QWE123", Account("David Rivera", "da489308"))
 
     print(vars(car0))
-    print(vars(car1))
+    print(vars(car0.driver))
