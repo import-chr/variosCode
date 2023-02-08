631bf9f157b216c95bd269503be17bf0fed67663
chirs <yosoypro_117@hotmail.com>
2023-02-08 12:17:29 -0600

    constructor method

--- POO_languages/JAVA/Main.java
+++ POO_languages/JAVA/Main.java
@@ -2,14 +2,10 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello Wolrd");
 
-        Car car0 = new Car();
-        car0.driver = "David";
-        car0.license = "QWE123";
+        Car car0 = new Car("QWE123", new Account("David", "da195465"));
         car0.passenger = 2;
 
-        Car car1 = new Car();
-        car1.driver = "Luis";
-        car1.license = "TRY987";
+        Car car1 = new Car("TRY987", new Account("Luis", "lu472930"));
         car1.passenger = 4;
 
         car0.printData();
