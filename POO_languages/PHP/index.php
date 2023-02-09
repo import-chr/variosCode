e6d88c49e059912566569e2a22a2c8fb1ed5ce39
chirs <yosoypro_117@hotmail.com>
2023-02-09 13:17:25 -0600

    uber x-pool objects

--- POO_languages/PHP/index.php
+++ POO_languages/PHP/index.php
@@ -2,6 +2,11 @@
 
 require_once("Car.php");
 require_once("Account.php");
+require_once("UberX.php");
+require_once("UberPool.php");
 
-$car0 = new Car("MFC277", new Account("Felipe Castellanos", "fe085903"));
-$car0->printDataCar();
+$uberx = new UberX("GCJ767", new Account("David Lopez", "da946103"), "Chevrolet", "Spark");
+$uberx->printDataCar();
+
+$uberpool = new UberPool("FDE754", new Account("Carlos Diaz", "ca730284"), "Dodge", "Attitude");
+$uberpool->printDataCar();
