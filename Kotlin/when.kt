82c9b0ba4a03894ecbe82e307142b2e08d19a3ef
chirs <yosoypro_117@hotmail.com>
2023-02-27 11:53:21 -0600

    when

---
+++ Kotlin/when.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val number = 42
+
+    val result = when {
+        num > 0 -> "Positivo"
+        num < 0 -> "Negativo"
+        else -> "Cero"
+    }
+
+    println(result)
+}
