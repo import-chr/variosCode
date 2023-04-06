984bfc07aa3d939dc3584d88dbe5ed17cff1326d
chirs <yosoypro_117@hotmail.com>
2023-04-06 13:52:33 -0600

    simple views

---
+++ PHP/frameworks/views/layout.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Title Page</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+        <div class="container">
+            <a href="/" class="navbar-brand h1">Framework</a>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <?php
+            echo $content;
+            ?>
+        </div>
+    </div>
+</body>
+</html>
