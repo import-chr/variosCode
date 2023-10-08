36a4b3fed25017f7bbd18b5d86e8380030da4da0
chirs <yosoypro_117@hotmail.com>
2023-10-07 20:46:49 -0600

    read n send

--- python/sockets/f_cliente.py
+++ python/sockets/f_cliente.py
@@ -16,17 +16,20 @@ cliente_socket.connect(direccion_servidor)
 
 #COMIENZA FASE DE COMUNICACION 
 try:
-    for i in range(5):
-        # Enviar datos al servidor
-        message = input() # "Hola, servidor"
-        cliente_socket.sendall(message.encode('utf-8'))
+    with open("cuento_cliente.txt", 'r') as cCliente:
+        lineas = cCliente.readlines()
+        for linea in lineas:
+            # Enviar datos al servidor
+            # message = input() # "Hola, servidor"
+            cliente_socket.sendall(linea.encode('utf-8'))
 
-        # Recibir respuesta del servidor
-        data = cliente_socket.recv(1024)
-        print(f"Respuesta del servidor: {data.decode('utf-8')}")
-
-except:
-    print ('\n Hubo un fallo en la fase de comunicacion')
+            # Recibir respuesta del servidor
+            data = cliente_socket.recv(1024)
+            print(f"Respuesta del servidor: {data.decode('utf-8')}")
+except FileNotFoundError:
+    print("El archivo 'cuento' no se encuentra")
+except Exception as e:
+    print(f"Se produjo un error: {str(e)}")
 
 # Cerrar la conexión
 cliente_socket.close()
