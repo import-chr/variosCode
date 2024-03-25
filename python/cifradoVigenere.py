262e2bae1f295dbecafd56aecdeb8e68af0f9933
Christian Loeza <yosoypro_117@hotmail.com>
2024-03-24 20:16:51 -0600

    type annotations on Vigenere
    type annotations with mypy

--- python/cifradoVigenere.py
+++ python/cifradoVigenere.py
@@ -1,30 +1,30 @@
 class CifradoVigenere:
     def __init__(self) -> None:
-        self.alphabet = "abcdefghijklmnopqrstuvwxyz"
-        self.alpha = len(self.alphabet)
+        self.alphabet: str = "abcdefghijklmnopqrstuvwxyz"
+        self.alpha: int = len(self.alphabet)
     
-    def getIdM(self, m, i = 0):
+    def getIdM(self, m: str, i: int = 0) -> int:
         return self.alphabet.find(m[i])
     
-    def fillArr(self, text):
-        arr = []
+    def fillArr(self, text: str) -> list[int]:
+        arr: list[int] = []
 
         for i in range(len(text)):
             arr.append(self.getIdM(text[i]))
         
         return arr
     
-    def toStr(self, arr):
-        arrC = []
+    def toStr(self, arr: list[int]) -> str:
+        arrC: list[str] = []
 
         for i in range(len(arr)):
             arrC.append(self.alphabet[arr[i] % self.alpha])
         
         return "".join(arrC)
     
-    def spaceStr(self, original, joined):
-        joined_spaces = []
-        joined_i = 0
+    def spaceStr(self, original: str, joined: str) -> str:
+        joined_spaces: list[str] = []
+        joined_i: int = 0
 
         for char in original:
             if char == " ":
@@ -35,11 +35,11 @@ class CifradoVigenere:
         
         return "".join(joined_spaces)
     
-    def cifrar(self, message, key):
-        arrMess = self.fillArr(message)
-        arrKey = self.fillArr(key)
-        arrCipher = []
-        j = 0
+    def cifrar(self, message: str, key: str) -> list[int]:
+        arrMess: list[int] = self.fillArr(message)
+        arrKey: list[int] = self.fillArr(key)
+        arrCipher: list[int] = []
+        j: int = 0
 
         for i in range(len(arrMess)):
             arrCipher.append((arrMess[i] + arrKey[j]) % self.alpha)
@@ -50,11 +50,11 @@ class CifradoVigenere:
         
         return arrCipher
     
-    def descifrar(self, message, key):
-        arrMess = self.fillArr(message)
-        arrKey = self.fillArr(key)
-        arrDescipher = []
-        j = 0
+    def descifrar(self, message: str, key: str) -> list[int]:
+        arrMess: list[int] = self.fillArr(message)
+        arrKey: list[int] = self.fillArr(key)
+        arrDescipher: list[int] = []
+        j: int = 0
 
         for i in range(len(arrMess)):
             arrDescipher.append((arrMess[i] - arrKey[j]) % self.alpha)
@@ -68,28 +68,28 @@ class CifradoVigenere:
 # ===================================
 # >>------  INSTANCIA  ------<<
 # ===================================
-vigenere = CifradoVigenere()
+vigenere: CifradoVigenere = CifradoVigenere()
 
 # ===================================
 # >>------  CIFRADO  ------<<
 # ===================================
-message = input("ingresa la frase a cifrar: ")
-key = input("ingresa la llave de cifrado: ")
+message: str = input("ingresa la frase a cifrar: ")
+key: str = input("ingresa la llave de cifrado: ")
 
-cipheredArr = vigenere.cifrar(message.replace(" ", ""), key)
-cipheredStr = vigenere.toStr(cipheredArr)
-ciphered = vigenere.spaceStr(message, cipheredStr)
+cipheredArr: list[int] = vigenere.cifrar(message.replace(" ", ""), key)
+cipheredStr: str = vigenere.toStr(cipheredArr)
+ciphered: str = vigenere.spaceStr(message, cipheredStr)
 
 print(f"vigenere (cifrado): {ciphered}")
 
 # ===================================
 # >>------  DESCIFRADO  ------<<
 # ===================================
-vigereneText = input("ingresa la frase a descifrar: ")
-desKey = input("ingresa la llave de descifrado: ")
+vigereneText: str = input("ingresa la frase a descifrar: ")
+desKey: str = input("ingresa la llave de descifrado: ")
 
-descipherArr = vigenere.descifrar(vigereneText.replace(" ", ""), desKey)
-descipherStr = vigenere.toStr(descipherArr)
-descipher = vigenere.spaceStr(vigereneText, descipherStr)
+descipherArr: list[int] = vigenere.descifrar(vigereneText.replace(" ", ""), desKey)
+descipherStr: str = vigenere.toStr(descipherArr)
+descipher: str = vigenere.spaceStr(vigereneText, descipherStr)
 
 print(f"vegenere (descifrado): {descipher}")
